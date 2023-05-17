47046d2232706fd89475aa955f40a4935c37ff8f
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-17 13:21:48 +0200

    Add queries to assign animals to owners

--- data.sql
+++ data.sql
@@ -11,3 +11,17 @@ INSERT INTO animals (id, name, date_of_birth, escape_attemps, neutered, weight_k
            (9, 'Boarmon', '2005-06-07', 7, true, 20.4),
            (10, 'Blossom', '1998-10-13', 3, true, 17),
            (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
+    VALUES  ('Sam Smith', 34),
+            ('Jennifer Orwel', 19),
+            ('Bob', 45),
+            ('Melody Pond', 77),
+            ('Dean Winchester', 14),
+            ('Jodie Whittaker', 38);
+
+UPDATE animals SET owner_id = ( SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+UPDATE animals SET owner_id = ( SELECT id FROM owners WHERE full_name = 'Jennifer Orwel') WHERE name IN ('Gabumon' ,'Pikachu');
+UPDATE animals SET owner_id = ( SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon' ,'Plantmon');
+UPDATE animals SET owner_id = ( SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander' ,'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = ( SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon' ,'Boarmon');
