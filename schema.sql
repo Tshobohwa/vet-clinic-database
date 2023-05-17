145b46cf854fd06e6d580309497831e327c73ebb
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-17 13:20:36 +0200

    Add owner and species tables

--- schema.sql
+++ schema.sql
@@ -8,3 +8,23 @@
  );
 
  ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+   id SERIAL PRIMARY KEY,
+   full_name VARCHAR(100),
+   age INT); 
+
+CREATE TABLE species (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(100));
+
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT species_id FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT owner_id FOREIGN KEY(owner_id) REFERENCES owners(id);
