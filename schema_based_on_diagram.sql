d1871677a6cbc6df8703fdccfb39a77a690c4c40
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-05-23 15:52:41 +0200

    Create treatments, invoices and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,3 +16,31 @@ CREATE TABLE medical_histories (
     FOREIGN KEY (patient_id) REFERENCES patients(Id)
 );
 
+---create treatement table
+CREATE TABLE treatments (
+	id INT PRIMARY KEY, 
+	type VARCHAR(255),
+	name VARCHAR(255)
+);
+
+---create invoices table
+CREATE TABLE invoices (
+	id INT PRIMARY KEY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INT,
+	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+);
+
+---create invoice_items table
+CREATE TABLE invoice_items (
+	id INT PRIMARY KEY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT,
+	treatment_id INT,
+	FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+	FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
