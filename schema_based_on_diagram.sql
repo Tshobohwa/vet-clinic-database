24258a2135a44134f406bad5161b227be9808133
Eduardo Villarreal <evillaz123@gmail.com>
2023-05-24 04:05:23 -0500

    Correct one to one relationship

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -29,7 +29,7 @@ CREATE TABLE invoices (
 	total_amount DECIMAL,
 	generated_at TIMESTAMP,
 	payed_at TIMESTAMP,
-	medical_history_id INT,
+	medical_history_id INT UNIQUE,
 	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
